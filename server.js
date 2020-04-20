22a917c922bcf7f6d86d7802f4093460acb187e1
John Moen <entity.john@gmail.com>
2020-04-20 14:04:22 -0500

    add changes from boardgame.io deployment docs

---
+++ server.js
@@ -0,0 +1,24 @@
+const Server = require('boardgame.io/server').Server
+const path = require('path')
+const Koa = require('koa')
+const serve = require('koa-static')
+const mount = require('koa-mount')
+const HexedMeadow = require('./src/game/game').HexedMeadow
+
+const server = Server({ games: [HexedMeadow] })
+const PORT = process.env.PORT || 3000
+const frontEndAppBuildPath = path.resolve(__dirname, './build')
+
+// Serve the build directory
+const static_pages = new Koa()
+static_pages.use(serve(frontEndAppBuildPath))
+server.app.use(mount('/', static_pages))
+server.run(PORT, () => {
+  server.app.use(
+    async (ctx, next) =>
+      await serve(frontEndAppBuildPath)(
+        Object.assign(ctx, { path: 'index.html' }),
+        next
+      )
+  )
+})
