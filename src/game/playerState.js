707a47f54d02757c26769ea65dee013b4192e190
John Moen <entity.john@gmail.com>
2020-08-15 13:57:56 -0500

    initialize variable of type string as empty string, not null

--- src/game/playerState.js
+++ src/game/playerState.js
@@ -1,18 +1,18 @@
 export const initialPlayerState = {
   '0': {
     orderMarkers: {
-      X: null,
-      '0': null,
-      '1': null,
-      '2': null,
+      X: '',
+      '0': '',
+      '1': '',
+      '2': '',
     },
   },
   '1': {
     orderMarkers: {
-      X: null,
-      '0': null,
-      '1': null,
-      '2': null,
+      X: '',
+      '0': '',
+      '1': '',
+      '2': '',
     },
   },
 }
@@ -20,19 +20,19 @@ export const prePlacedOrderMarkers = {
   '0': {
     orderMarkers: {
       // All on 1st Card
-      X: 'p0cIDhm103n0',
-      '0': 'p0cIDhm103n0',
-      '1': 'p0cIDhm103n0',
-      '2': 'p0cIDhm103n0',
+      X: 'p0_hm103_0',
+      '0': 'p0_hm103_0',
+      '1': 'p0_hm103_0',
+      '2': 'p0_hm103_0',
     },
   },
   '1': {
     orderMarkers: {
       // All on 1st Card
-      X: 'p1cIDhm203n0',
-      '0': 'p1cIDhm203n0',
-      '1': 'p1cIDhm203n0',
-      '2': 'p1cIDhm203n0',
+      X: 'p1_hm203_0',
+      '0': 'p1_hm203_0',
+      '1': 'p1_hm203_0',
+      '2': 'p1_hm203_0',
     },
   },
 }
