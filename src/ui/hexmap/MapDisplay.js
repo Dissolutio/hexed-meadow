db0325e46cd52d0d0497fcc8cc41d35ecfce3670
John Moen <entity.john@gmail.com>
2020-09-21 12:23:21 -0500

    remove old viewbox and computed map svg stuff

--- src/ui/hexmap/MapDisplay.js
+++ src/ui/hexmap/MapDisplay.js
@@ -11,10 +11,7 @@ export const MapDisplay = () => {
     isPlacementPhase,
     isRoundOfPlayPhase,
   } = useBoardContext()
-  const {
-    onClickMapBackground__turn,
-    boardHexesToHighlight_selectedUnits,
-  } = useTurnContext()
+  const { onClickMapBackground__turn } = useTurnContext()
   const { onClickMapBackground__placement } = usePlacementContext()
 
   const handleClickMapBackground = () => {
@@ -26,32 +23,10 @@ export const MapDisplay = () => {
     }
   }
 
-  const computedViewBox = () => {
-    const phi = hexMap.hexWidth
-    const Xo = -2 * phi * hexMap.mapSize
-    const Yo = -2 * phi * hexMap.mapSize
-    const Xt = 4 * phi * hexMap.mapSize
-    const Yt = 4 * phi * hexMap.mapSize
-    return `${Xo} ${Yo} ${Xt} ${Yt}`
-  }
-
   return (
     <HexSVGStyle onClick={handleClickMapBackground} pID={playerID}>
-      <HexGrid
-        // width="500px"
-        // height="500px"
-        // viewBox={`0 0 360 360`}
-        //
-        // width={`${100 * (1 + Math.floor(hexMap.mapSize / 5))}%`}
-        // height={`${100 * (1 + Math.floor(hexMap.mapSize / 5))}%`}
-        // no viewbox
-        //
-        width={`100%`}
-        height={`100%`}
-        viewBox={computedViewBox()}
-      >
+      <HexGrid width={`100%`} height={`100%`}>
         <Layout
-          // size={{ x: `${hexMap.hexHeight}`, y: `${hexMap.hexHeight}` }}
           size={{
             x: `${(10 * hexMap.hexWidth) / hexMap.mapSize}`,
             y: `${(10 * hexMap.hexHeight) / hexMap.mapSize}`,
