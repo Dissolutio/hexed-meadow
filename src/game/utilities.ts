b8376520d36725e08fba32dd2d4d117050e119cf
John Moen <entity.john@gmail.com>
2020-11-07 10:09:48 -0600

    tidy

--- src/game/utilities.ts
+++ src/game/utilities.ts
@@ -1,13 +1,6 @@
 import { GameUnit, MoveRange } from './startingUnits'
 import { BoardHex } from './mapGen'
 
-export function getRandomInt(min: number, max: number) {
-  //* max and min inclusive
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
 export function cloneObject(
   obj: { [key: string]: Object } | GameUnit | BoardHex
 ): any {
