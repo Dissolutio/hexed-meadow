de8ea014fe01216a3903d6634d50f0c7a1223a17
John Moen <entity.john@gmail.com>
2020-11-17 23:02:31 -0600

    extract MapHexStyles into own component

--- src/ui/hexmap/MapDisplay.tsx
+++ src/ui/hexmap/MapDisplay.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
-import styled from 'styled-components'
 import { useBoardContext } from 'ui/hooks'
 import { ReactHexgrid } from './ReactHexgrid'
+import { MapHexStyles } from './MapHexStyles'
 import { MapHexes } from './MapHexes'
 import { TurnCounter } from './TurnCounter'
 import { ZoomControls } from './ZoomControls'
@@ -48,7 +48,7 @@ export const MapDisplay = () => {
         handleClickZoomOut={handleClickZoomOut}
       />
       <TurnCounter />
-      <StyledReactHexgrid hexSize={mapState.hexSize} ref={mapRef}>
+      <MapHexStyles hexSize={mapState.hexSize} ref={mapRef}>
         <ReactHexgrid
           mapSize={mapSize}
           width={`${mapState.width}%`}
@@ -58,121 +58,7 @@ export const MapDisplay = () => {
         >
           <MapHexes hexSize={mapState.hexSize} />
         </ReactHexgrid>
-      </StyledReactHexgrid>
+      </MapHexStyles>
     </div>
   )
 }
-type StyledReactHexgridProps = {
-  hexSize: number
-}
-const StyledReactHexgrid = styled.div<StyledReactHexgridProps>`
-  height: 100%;
-  position: relative;
-  overflow: scroll;
-  //🛠 Targets the react-hexgrid svg wrapper
-  svg.grid {
-    position: absolute;
-    top: 0%;
-    left: 0%;
-  }
-  //🛠 Style scrollbars
-  scrollbar-width: thin;
-  scrollbar-color: var(--player-color) var(--black);
-  &::-webkit-scrollbar {
-    height: 0.2rem;
-    width: 0.2rem;
-    background: var(--black);
-  }
-  &::-webkit-scrollbar-track {
-    border-radius: 10px;
-    box-shadow: inset 0 0 1px var(--player-color);
-    background: var(--black);
-  }
-  &::-webkit-scrollbar-thumb {
-    background: var(--player-color);
-    border-radius: 10px;
-  }
-  &::-webkit-scrollbar-corner {
-    background: var(--black);
-  }
-
-  //🛠 Style Hex Text
-  .maphex_altitude-text {
-    fill: var(--sub-white);
-    font-size: ${(props) => `${props.hexSize / 75}rem`};
-  }
-  //🛠 Terrain Highlighting
-  .maphex__terrain--grass > g polygon {
-    fill: var(--neon-green);
-    fill-opacity: 0.2;
-  }
-
-  //🛠 Hex Highlighting
-
-  /* HIGHLIGHT ALL HEXES */
-  svg g polygon {
-    stroke: var(--player-color);
-    stroke-width: 0.01;
-  }
-
-  /* PAINT ALL HEXES */
-  .hexagon-group {
-    fill: var(--black);
-    g polygon {
-      @media (hover: hover) {
-        &:hover {
-          fill: var(--neon-orange);
-          fill-opacity: 0.6;
-        }
-      }
-    }
-  }
-
-  /* HIGHLIGHT STARTZONE HEX */
-  .maphex__start-zone--placement > g polygon {
-    stroke: var(--player-color);
-    stroke-width: 0.6;
-  }
-  /* HIGHLIGHT SELECTED HEXES */
-  .maphex__selected--active > g polygon {
-    stroke: var(--white);
-    stroke-width: 0.6;
-  }
-  /* HIGHLIGHT SELECTABLE UNITS */
-  .maphex__selected-card-unit--selectable > g polygon {
-    stroke: var(--sub-white);
-    stroke-width: 0.6;
-  }
-  /* HIGHLIGHT SELECTED UNIT */
-  .maphex__selected-card-unit--active > g polygon {
-    stroke: var(--player-color);
-    stroke-width: 0.6;
-  }
-  /* HIGHLIGHT ACTIVE ENEMY UNIT */
-  .maphex__opponents-active-unit > g polygon {
-    stroke: var(--neon-red);
-    stroke-width: 0.6;
-  }
-  //🛠 PAINT MOVE HEXES
-  /* PAINT SAFE MOVERANGE */
-  .maphex__move-safe > g polygon {
-    fill: var(--neon-green);
-    fill-opacity: 1;
-  }
-  /* PAINT ENGAGE MOVERANGE */
-  .maphex__move-engage > g {
-    fill: var(--neon-orange);
-    fill-opacity: 1;
-  }
-  /* PAINT DISENGAGE MOVERANGE */
-  .maphex__move-disengage > g {
-    fill: var(--neon-red);
-    fill-opacity: 1;
-  }
-  //🛠 PAINT ATTACK HEXES
-  /* PAINT TARGETABLE ENEMY UNIT */
-  .maphex__targetable-enemy > g polygon {
-    fill: var(--neon-red);
-    fill-opacity: 1;
-  }
-`
---
+++ src/ui/hexmap/MapHexStyles.ts
@@ -0,0 +1,117 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type MapHexStylesProps = {
+  hexSize: number
+}
+export const MapHexStyles = styled.div<MapHexStylesProps>`
+  height: 100%;
+  position: relative;
+  overflow: scroll;
+  //🛠 Targets the react-hexgrid svg wrapper
+  svg.grid {
+    position: absolute;
+    top: 0%;
+    left: 0%;
+  }
+  //🛠 Style scrollbars
+  scrollbar-width: thin;
+  scrollbar-color: var(--player-color) var(--black);
+  &::-webkit-scrollbar {
+    height: 0.2rem;
+    width: 0.2rem;
+    background: var(--black);
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 10px;
+    box-shadow: inset 0 0 1px var(--player-color);
+    background: var(--black);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: var(--player-color);
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-corner {
+    background: var(--black);
+  }
+
+  //🛠 Style Hex Text
+  .maphex_altitude-text {
+    fill: var(--sub-white);
+    font-size: ${(props) => `${props.hexSize / 75}rem`};
+  }
+  //🛠 Terrain Highlighting
+  .maphex__terrain--grass > g polygon {
+    fill: var(--neon-green);
+    fill-opacity: 0.2;
+  }
+
+  //🛠 Hex Highlighting
+
+  /* HIGHLIGHT ALL HEXES */
+  svg g polygon {
+    stroke: var(--player-color);
+    stroke-width: 0.01;
+  }
+
+  /* PAINT ALL HEXES */
+  .hexagon-group {
+    fill: var(--black);
+    g polygon {
+      @media (hover: hover) {
+        &:hover {
+          fill: var(--neon-orange);
+          fill-opacity: 0.6;
+        }
+      }
+    }
+  }
+
+  /* HIGHLIGHT STARTZONE HEX */
+  .maphex__start-zone--placement > g polygon {
+    stroke: var(--player-color);
+    stroke-width: 0.6;
+  }
+  /* HIGHLIGHT SELECTED HEXES */
+  .maphex__selected--active > g polygon {
+    stroke: var(--white);
+    stroke-width: 0.6;
+  }
+  /* HIGHLIGHT SELECTABLE UNITS */
+  .maphex__selected-card-unit--selectable > g polygon {
+    stroke: var(--sub-white);
+    stroke-width: 0.6;
+  }
+  /* HIGHLIGHT SELECTED UNIT */
+  .maphex__selected-card-unit--active > g polygon {
+    stroke: var(--player-color);
+    stroke-width: 0.6;
+  }
+  /* HIGHLIGHT ACTIVE ENEMY UNIT */
+  .maphex__opponents-active-unit > g polygon {
+    stroke: var(--neon-red);
+    stroke-width: 0.6;
+  }
+  //🛠 PAINT MOVE HEXES
+  /* PAINT SAFE MOVERANGE */
+  .maphex__move-safe > g polygon {
+    fill: var(--neon-green);
+    fill-opacity: 1;
+  }
+  /* PAINT ENGAGE MOVERANGE */
+  .maphex__move-engage > g {
+    fill: var(--neon-orange);
+    fill-opacity: 1;
+  }
+  /* PAINT DISENGAGE MOVERANGE */
+  .maphex__move-disengage > g {
+    fill: var(--neon-red);
+    fill-opacity: 1;
+  }
+  //🛠 PAINT ATTACK HEXES
+  /* PAINT TARGETABLE ENEMY UNIT */
+  .maphex__targetable-enemy > g polygon {
+    fill: var(--neon-red);
+    fill-opacity: 1;
+  }
+`
